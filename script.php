82fb00328d1391a42f0c8c8f1dbf0d9428e26043
Danila Buyanov <info@saity74.ru>
2015-07-02 02:35:05 +0500

    Update script.php

--- script.php
+++ script.php
@@ -1,76 +1,72 @@
 <?php
-// No direct access to this file
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_catalogue
+ *
+ * @copyright   Copyright (C) 20012 - 2015 Saity74, LLC. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die('Restricted access');
 
 /**
- * Script file of HelloWorld component
+ * Class Com_CatalogueInstallerScript
+ *
+ * @since   1.5
  */
-class com_catalogueInstallerScript
+class Com_CatalogueInstallerScript
 {
-    /**
-     * method to install the component
-     *
-     * @return void
-     */
-    function install($parent)
-    {
-        // Create categories for our component
-        $basePath = JPATH_ADMINISTRATOR . '/components/com_categories';
-        require_once $basePath . '/models/category.php';
-        $config = array( 'table_path' => $basePath . '/tables');
-        $model = new CategoriesModelCategory($config);
-        $data = array( 'id' => 0, 'parent_id' => 1, 'level' => 1, 'path' => 'uncategorised', 'extension' => 'com_catalogue' , 'title' => 'Uncategorised', 'alias' => 'uncategorised', 'published' => 1, 'language' => '*');
-        $status = $model->save($data);
+	/**
+	 * Method to install an extension.
+	 *
+	 * @return  void
+	 */
+	function install()
+	{
+		// Create categories for our component
+		$basePath = JPATH_ADMINISTRATOR . '/components/com_categories';
 
-        if(!$status)
-        {
-            JError::raiseWarning(500, JText::_('Unable to create default content category!'));
-        }
-    }
+		/** @noinspection PhpIncludeInspection */
+		require_once $basePath . '/models/category.php';
+		$config = array('table_path' => $basePath . '/tables');
+		$model = new CategoriesModelCategory($config);
+		$data = array(
+			'id' => 0,
+			'parent_id' => 1,
+			'level' => 1,
+			'path' =>'uncategorised',
+			'extension' => 'com_catalogue',
+			'title' => 'Uncategorised',
+			'alias' => 'uncategorised',
+			'published' => 1,
+			'language' => '*'
+		);
+		$status = $model->save($data);
 
-    /**
-     * method to uninstall the component
-     *
-     * @return void
-     */
-    function uninstall($parent)
-    {
-        // $parent is the class calling this method
-        echo '<p>' . JText::_('COM_CATALOGUE_UNINSTALL_TEXT') . '</p>';
-    }
+		if (!$status)
+		{
+			JError::raiseWarning(500, JText::_('Unable to create default content category!'));
+		}
+	}
 
-    /**
-     * method to update the component
-     *
-     * @return void
-     */
-    function update($parent)
-    {
-        // $parent is the class calling this method
-        echo '<p>' . JText::sprintf('COM_CATALOGUE_UPDATE_TEXT', $parent->get('manifest')->version) . '</p>';
-    }
+	/**
+	 * method to uninstall the component
+	 *
+	 * @return  void
+	 */
+	function uninstall()
+	{
+		echo '<p>' . JText::_('COM_CATALOGUE_UNINSTALL_TEXT') . '</p>';
+	}
 
-    /**
-     * method to run before an install/update/uninstall method
-     *
-     * @return void
-     */
-    function preflight($type, $parent)
-    {
-        // $parent is the class calling this method
-        // $type is the type of change (install, update or discover_install)
-        // echo '<p>' . JText::_('COM_CATALOGUE_PREFLIGHT_' . $type . '_TEXT') . '</p>';
-    }
-
-    /**
-     * method to run after an install/update/uninstall method
-     *
-     * @return void
-     */
-    function postflight($type, $parent)
-    {
-        // $parent is the class calling this method
-        // $type is the type of change (install, update or discover_install)
-        // echo '<p>' . JText::_('COM_CATALOGUE_POSTFLIGHT_' . $type . '_TEXT') . '</p>';
-    }
+	/**
+	 * method to update the component
+	 *
+	 * @param   object  $parent  Object
+	 *
+	 * @return  void
+	 */
+	function update($parent)
+	{
+		echo '<p>' . JText::sprintf('COM_CATALOGUE_UPDATE_TEXT', $parent->get('manifest')->version) . '</p>';
+	}
 }
